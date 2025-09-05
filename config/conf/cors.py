f0398384e02bbc9223cb44b1c833b2045886f65b
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-09-05 22:12:54 +0500

    add domain for cors

--- config/conf/cors.py
+++ config/conf/cors.py
@@ -2,8 +2,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
-    'https://agro360.vercel.app',
-    'https://agro360-admin.vercel.app',
+    'https://agro365.vercel.app',
+    'https://agro365-admin.vercel.app',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
