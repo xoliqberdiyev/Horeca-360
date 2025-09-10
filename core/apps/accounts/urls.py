4a2aff7d4925e86e4ccd934bc34ceca0f1032394
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-09-10 18:02:01 +0500

    fix

--- core/apps/accounts/urls.py
+++ core/apps/accounts/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 
 from rest_framework_simplejwt.views import TokenObtainPairView
 from core.apps.accounts.views import like as like_views
+from core.apps.accounts.views import user
 
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view()),    
+    path('login/', user.UserLoginApiView.as_view()),    
     path('<uuid:product_id>/like/', like_views.LikeApiView.as_view()),
     path('liked_products/', like_views.LikeProductListApiView.as_view()),
 ]
