472956e00e0e2708080446f0d60b3ea1606c2ab6
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-09-10 17:52:08 +0500

    add: add tg_id

--- core/apps/accounts/admin/user.py
+++ core/apps/accounts/admin/user.py
@@ -1,9 +1,35 @@
 from django.contrib import admin 
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core.apps.accounts.models import User
 
 
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
-    pass
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", 'tg_id')}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "usable_password", "password1", "password2"),
+            },
+        ),
+    )
